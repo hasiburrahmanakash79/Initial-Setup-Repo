8fdb16b16002c457cf87fe2bdc15efa746730ebc
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-04-21 10:20:26 +0600

    make a initial project setup repo

---
+++ src/Pages/Authentication/Signup.jsx
@@ -0,0 +1,27 @@
+
+
+const Signup = () => {
+
+    return (
+        <div>
+            <h1>Signup</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    );
+};
+
+export default Signup;
---
+++ src/Routers/Router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+import Main from "../Layouts/Main";
+import Login from "../Pages/Authentication/Login";
+import Signup from "../Pages/Authentication/Signup";
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>,
+      children: [
+        {
+          path: "/",
+          element: <Login/>
+        },
+        {
+          path: "/signup",
+          element: <Signup/>
+        }
+      ],
+    },
+  ]);
+  
+  export default router;
